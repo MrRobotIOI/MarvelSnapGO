62dd6d1f65f073ac9252d4c790063ca75d0d5e87
Zed <zimikejiani@gmail.com>
2024-04-30 18:59:57 -0400

    first commit

--- src/app/cards/page.tsx
+++ src/app/cards/page.tsx
@@ -1,47 +1,40 @@
 import Link from "next/link";
 
- async function getCards(){
+async function getCards(){
     const res = await fetch(
-        'http://localhost:5289/*api/WarHammer',
-
+        'http://localhost:5289/api/WarHammer',
         { cache: 'no-store'}
-);
+    );
     const data = await res.json();
-
     return data;
 }
 
 export default async function CardsPage(){
     const cards : any[]= await getCards();
-console.log(cards);
+    console.log(cards);
 
-return (
-    <div>
-        <h1>Cards</h1>
-
+    return (
         <div>
-            {
-                cards?.map((card)=>{
-
-                    return <Card key = {card.id} card={card} />;
-                })
-            }
+            <h1>Cards</h1>
+            <div>
+                {cards?.map((card)=>{
+                    return <Card key={card.id} card={card} />;
+                })}
+            </div>
         </div>
-    </div>
-);
+    );
 }
 
 function Card({card}: any) {
-    const {id, name, description, cardinfolink,image} = card ||{};
+    const {id, name, description, cardinfolink, image} = card || {};
 
     return(
-
         <Link href={`/cards/${id}`} passHref>
-            <img src={"https://static.marvelsnap.pro/cards/Mockingbird_"+image[0]+".webp"} alt={name}/>
+            <img src={`https://static.marvelsnap.pro/cards/Mockingbird_${image[0]}.webp`} alt={name}/>
             <div>
                 <h2>{name}</h2>
                 <h5>{description}</h5>
-<h4>{process.env.customKey}</h4>
+                <h4>{process.env.customKey}</h4>
             </div>
         </Link>
     );
